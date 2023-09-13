67cc5865c56090c06b601a8396cc3beaef56c809
Munchquim <qpinedasotes@gmail.com>
2023-09-13 06:05:03 +0200

    añadido img de tipos y buscador

--- src/main.js
+++ src/main.js
@@ -3,26 +3,37 @@ const POKE_ID_MAX = 151;
 const LINK = "https://pokeapi.co/api/v2/pokemon/";
 const IMG_LINK = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
 
+
+
 makeCards ();
 //rellena las cartas//
 async function getPokemon (id){
     const POKE_NAME = document.getElementById("name"+id);
     const POKE_PICTURE = document.getElementById("picture"+id);
     const POKE_NUM = document.getElementById("number"+id);
+    const POKE_CARD = document.getElementById("card"+id);
 
     const RESPUESTA = await fetch (LINK+id)
     const DATA = await RESPUESTA.json();
     POKE_NAME.innerText = DATA.forms[0].name;
     POKE_PICTURE.src = DATA.sprites.front_default;
     POKE_NUM.innerText = DATA.id;
+    
+    //añade los tipos como class//
+    for (let index = 0; index < DATA.types.length; index++) {
+        POKE_CARD.classList.add(""+DATA.types[index].type.name+"");    
+    }
+    
 }
 //---------------------------------//
 
 //crea las cartas//
 async function makeCards (){
     for (let index = POKE_ID_MIN; index <= POKE_ID_MAX; index++) { 
+
         let newDiv = document.createElement("div");
         newDiv.setAttribute("class", "flex flex-col h-40 justify-between bg-orange-400 rounded p-2 m-2");
+        newDiv.setAttribute("id", "card"+index)
 
         let nameDiv = document.createElement("div");
         nameDiv.setAttribute("id", "name"+index);
@@ -47,10 +58,25 @@ async function makeCards (){
         document.getElementById("card_container").appendChild(newDiv);
 
         await getPokemon(index);
+        
     }
 }
 //---------------------------------//
 
-//estandarizacion tamañano, cutre//
+//evento de busqueda//
+function search() {
+    const searchTerm = document.getElementById("browser").value.toLowerCase(); // Obtén el valor del input de búsqueda en minúsculas
+    
+    for (let index = POKE_ID_MIN; index <= POKE_ID_MAX; index++) {
+        const card = document.getElementById("card" + index);
+        const cardName = document.getElementById("name" + index).textContent.toLowerCase();
 
+        // Comprueba si el nombre de la tarjeta contiene el término de búsqueda
+        if (cardName.includes(searchTerm)) {
+            card.style.display = "flex"; // Muestra la tarjeta si coincide con la búsqueda
+        } else {
+            card.style.display = "none"; // Oculta la tarjeta si no coincide con la búsqueda
+        }
+    }
+}
 //---------------------------------//
