5069ac600a9e460f59a21eaca03a9d24d50111b7
Munchquim <qpinedasotes@gmail.com>
2023-09-19 20:57:35 +0200

    pantalla de carga

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const POKE_ID_MIN = 1;
 const POKE_ID_MAX = 649;
+let counter = POKE_ID_MAX;
 
 const LINK = "https://pokeapi.co/api/v2/pokemon/";
 const IMG_LINK = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
@@ -12,6 +13,10 @@ const TYPE_ARRAY = ["./img/Tipo_normal_icono_EP.png","./img/Tipo_lucha_icono_EP.
 
 const TYPE_MAX_SIZE = "1.15";
 const TYPE_MIN_SIZE = "0.85";
+
+const COLOR_ARRAY = ["#9fa19f","#ff8000","#81b9ef","#8f41cb","#915121","#915121","#91a119","#704170","#60a1b8","#e62829","#2980ef","#3fa129","#fac000","#ef4179","#3fd8ff","#5061e1","#50413f","#ef71ef"];
+
+let cardBackgroundSwitch = true;//interruptor "fisico" no implementado
 
 makeCards ();
 //rellena las cartas//
@@ -27,10 +32,11 @@ async function getPokemon (id){
     POKE_PICTURE.src = DATA.sprites.front_default;
     POKE_NUM.innerText = DATA.id;
     
+    let aSwitch = true;
     //añade los tipos como class//
     for (let index = 0; index < DATA.types.length; index++) {
         //obtener la id del tipo//
-        const TYPE_RESPONSE = await fetch (DATA.types[index].type.url)
+        const TYPE_RESPONSE = await fetch (DATA.types[index].type.url);
         const DATA_TYPE = await TYPE_RESPONSE.json();
         //------//
 
@@ -42,24 +48,35 @@ async function getPokemon (id){
         let pokeType = document.createElement("img");
         pokeType.setAttribute("src", TYPE_ARRAY[(DATA_TYPE.id)-1]);
         pokeType.setAttribute("alt", "imagen del tipo "+DATA.types[index].type.name);
-        pokeType.setAttribute("class","h-4 w-4 ")
+        pokeType.setAttribute("class","h-4 w-4 ");
+        
         //------// 
+
+        //segun cardBackgroundSwitch cambia el color de la card por el del tipo//
+        
+        if (cardBackgroundSwitch && aSwitch) {
+            document.getElementById("card"+id).style.backgroundColor = ""+COLOR_ARRAY[(DATA_TYPE.id)-1];
+        }
+        aSwitch = false;//seberia ser true solo una vez por carta.
+        //------//
         
         document.getElementById("typeContainer"+id).appendChild(pokeType);
     }
-    
-
-
+    counter -= 1;// importante para la pantalla de carga
+    if (counter <= 0) {
+        animateLS();
+    }
 }
 //---------------------------------//
 
 //crea las cartas//
 async function makeCards (){
+    makeLS(); 
     for (let index = POKE_ID_MIN; index <= POKE_ID_MAX; index++) { 
 
         let newDiv = document.createElement("div");
         newDiv.setAttribute("class", "");
-        newDiv.setAttribute("id", "card"+index)
+        newDiv.setAttribute("id", "card"+index);
 
         let nameDiv = document.createElement("div");
         nameDiv.setAttribute("id", "name"+index);
@@ -88,9 +105,10 @@ async function makeCards (){
 
         document.getElementById("card_container").appendChild(newDiv);
 
-        await getPokemon(index); // quitar este await puede hacer que vaya mas rapido?
+        /* await */ getPokemon(index); // quitar este await puede hacer que vaya mas rapido?
         
     }
+    
 }
 //---------------------------------//
 
@@ -127,9 +145,6 @@ function selectType(id){
    search();
 
 }
-/*parte 1 marcar la imagen*/
-
-//---------------------------------//
 
 //evento de busqueda//
 function search() {
@@ -153,4 +168,30 @@ function search() {
     }
 }
 //---------------------------------//
+
+let loadingScreen = document.getElementById("loadingScreen");
+//Crear pantalla de carga//
+function makeLS() {
+    document.body.style.overflow = "hidden";
+    document.getElementById("lsImg").src = "./img/pokeball.PNG"; //redundante pero por si algun dia quiero que pueda haber varias imagenes de carga aleatorias 
+}
+//---------------------------------//
+
+//animacion de borrado de pantalla de carga//
+function animateLS() {
+    document.getElementById("lsImg").style.animation = "salida 1.5s ease-in-out ";
+    loadingScreen.style.animation = "bajada 1.5s ease ";
+
+    document.getElementById("lsImg").addEventListener("animationend", () => {
+        document.body.style.overflow = "visible";
+        loadingScreen.parentNode.removeChild(loadingScreen);
+    });
+}
+//---------------------------------//
+
+//borrar pantalla de carga//
+
+
+//---------------------------------//
+
 /* searchTypeArray.some(tipo =>card.className.includes(tipo)) */
